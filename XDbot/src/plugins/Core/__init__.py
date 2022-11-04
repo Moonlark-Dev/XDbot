32ceb6b988f0a51c7a8621e87894357ba40c966f
This-is-XiaoDeng <xiaodeng@thisisxd.tk>
2022-11-04 22:45:13 +0800

    [update]随机图片插件和自动文件检查

--- XDbot/src/plugins/Core/__init__.py
+++ XDbot/src/plugins/Core/__init__.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Import modules
 from . import status
+from . import st
+
+# Import libraries
+from nonebot.log import logger
+import os
+import os.path
+import json
+
+# Check files
+path = os.path.dirname(os.path.abspath(__file__))
+files = json.load(open(os.path.join(path, "init.json")))
+for file in files:
+    if file["is_dir"]:
+        logger.info(f"Checking directory: {file['path']} . . .")
+        if not os.path.isdir(file["path"]):
+            os.mkdir(file["path"])
+    else:
+        logger.info(f"Checking file: {file['path']}")
+        if not os.path.isfile(file["path"]):
+            with open(file["path"], "w", encoding="utf-8") as f:
+                f.write(file["default"])
+logger.info("Done")
