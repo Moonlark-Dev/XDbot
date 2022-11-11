ab8481121002249a1a9fc3a5f4e78197580ea397
XieXiLin <xiexilin158@qq.com>
2022-11-11 21:19:46 +0800

    [update]未测试的提交

--- XDbot/src/plugins/Core/ping.py
+++ XDbot/src/plugins/Core/ping.py
@@ -27,22 +27,30 @@ class new_stdout:
         return self.print_text
 """
 
-
 async def ping(url):
-    cmd  = os.popen(f"ping \"{url}\" {config.ping.count_arg} {config.ping.count}")
     # TODO ping不阻塞线程
     # newstdout = new_stdout()
     # p = subprocess.run(["ping",url,config.ping.count_arg,str(config.ping.count)],
     #                      stdout=newstdout)
+    #
     # text = newstdout.get()
-    text = cmd.read()
+    # cmd  = os.popen(f"ping \"{url}\" {count_arg} 4")
+    # text = cmd.read()
+    # answ = text.split("\n\n")[1]
+    # await commands.ping.finish("\n" + answ, at_sender = True)
+    cmd = await asyncio.create_subprocess_shell(f"ping \"{url}\" {count_arg} 4", stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await cmd.communicate()
+    text = stdout.decode()
     answ = text.split("\n\n")[1]
     await commands.ping.finish("\n" + answ, at_sender = True)
 
-
 async def ping_full_log(url):
-    cmd  = os.popen(f"ping \"{url}\" {count_arg} 4")
-    text = cmd.read()
+    # cmd  = os.popen(f"ping \"{url}\" {count_arg} 4")
+    # text = cmd.read()
+    # await commands.ping_full_log.finish("\n" + text, at_sender = True)
+    cmd = await asyncio.create_subprocess_shell(f"ping \"{url}\" {count_arg} 4", stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await cmd.communicate()
+    text = stdout.decode()
     await commands.ping_full_log.finish("\n" + text, at_sender = True)
 
 
