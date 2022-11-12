7b7584cb301876e9d9f508b31ff222d4ca91cc18
This-is-XiaoDeng <xiaodeng@thisisxd.tk>
2022-11-12 09:53:21 +0800

    [fix]ping插件的一个bug

--- XDbot/src/plugins/Core/ping.py
+++ XDbot/src/plugins/Core/ping.py
@@ -1,11 +1,8 @@
-from nonebot.log import logger
 from . import __config__ as config
 from . import __commands__ as commands
 import nonebot.adapters.onebot.v11.message
 import nonebot.adapters
 import nonebot.params
-import os
-# import subprocess
 import asyncio
 
 """
@@ -27,31 +24,23 @@ class new_stdout:
         return self.print_text
 """
 
+
 async def ping(url):
-    # TODO ping不阻塞线程
-    # newstdout = new_stdout()
-    # p = subprocess.run(["ping",url,config.ping.count_arg,str(config.ping.count)],
-    #                      stdout=newstdout)
-    #
-    # text = newstdout.get()
-    # cmd  = os.popen(f"ping \"{url}\" {count_arg} 4")
-    # text = cmd.read()
-    # answ = text.split("\n\n")[1]
-    # await commands.ping.finish("\n" + answ, at_sender = True)
-    cmd = await asyncio.create_subprocess_shell(f"ping \"{url}\" {count_arg} 4", stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    cmd = await asyncio.create_subprocess_shell(f"ping \"{url}\" {config.ping.count_arg} {config.ping.count}", stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await cmd.communicate()
     text = stdout.decode()
     answ = text.split("\n\n")[1]
-    await commands.ping.finish("\n" + answ, at_sender = True)
+    await commands.ping.finish("\n" + answ, at_sender=True)
+
 
 async def ping_full_log(url):
     # cmd  = os.popen(f"ping \"{url}\" {count_arg} 4")
     # text = cmd.read()
     # await commands.ping_full_log.finish("\n" + text, at_sender = True)
-    cmd = await asyncio.create_subprocess_shell(f"ping \"{url}\" {count_arg} 4", stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    cmd = await asyncio.create_subprocess_shell(f"ping \"{url}\" {config.ping.count_arg} {config.ping.count}", stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await cmd.communicate()
     text = stdout.decode()
-    await commands.ping_full_log.finish("\n" + text, at_sender = True)
+    await commands.ping_full_log.finish("\n" + text, at_sender=True)
 
 
 @commands.ping.handle()
@@ -59,7 +48,7 @@ async def ping_handle(
     args: nonebot.adapters.onebot.v11.message.Message = nonebot.params.CommandArg()
 ):
     await commands.ping.send(
-        f"正在 PING {args}，请稍候", at_sender = True)
+        f"正在 PING {args}，请稍候", at_sender=True)
     asyncio.create_task(ping(args))
 
 
@@ -68,5 +57,5 @@ async def ping_full_log_handle(
     args: nonebot.adapters.onebot.v11.message.Message = nonebot.params.CommandArg()
 ):
     await commands.ping_full_log.send(
-        f"正在 PING {args}，请稍候", at_sender = True)
-    asyncio.create_task(ping_full_log(args))  
+        f"正在 PING {args}，请稍候", at_sender=True)
+    asyncio.create_task(ping_full_log(args))
