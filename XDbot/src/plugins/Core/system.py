caa3d5c92399eafc52b2c1e8e68630e373ebc150
XieXiLin <xiexilin158@qq.com>
2022-11-13 09:32:06 +0800

    [update]添加错误输出方法

--- XDbot/src/plugins/Core/system.py
+++ XDbot/src/plugins/Core/system.py
@@ -12,5 +12,5 @@ async def system_handle(args: nonebot.adapters.onebot.v11.message.Message = none
     command = args.extract_plain_text()
     cmd = await asyncio.create_subprocess_shell(command, stdin=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await cmd.communicate()
-    text = stdout.decode()
+    text = "[stdout]\n" + stdout.decode() + "\n[stderr]\n" + stderr.decode()
     await commands.system.finish(text)
